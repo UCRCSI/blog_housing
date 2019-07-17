94fac9e9ed3f38d9c37991490205d873a50c439c
SunnyYShao <yshao006@ucr.edu>
2019-07-17 16:21:03 -0700

    example for generating median household income

---
+++ example median.R
@@ -0,0 +1,10 @@
+dta <- read_rds("pums_final.rds") %>%
+  mutate(statefip = as_factor(lbl_clean(statefip)))
+
+income$ownershp
+pums_weighted <- income %>%
+  as_survey_design(ids = 1,weight = perwt)
+
+pums_weighted %>%
+  group_by(aapi_alone) %>%
+  summarize(median_income = survey_median(income_2017,na.rm = T)) -> final_dta1
