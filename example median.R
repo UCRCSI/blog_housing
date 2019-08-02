363a3e611843e8662090aadb497cb681826d13eb
SunnyYShao <yshao006@ucr.edu>
2019-08-02 16:05:10 -0700

    update save excel r script

--- example median.R
+++ example median.R
@@ -12,4 +12,12 @@ pums_weighted %>%
   group_by(aapi_alone) %>%
   summarize(median_income = survey_median(income_2017,na.rm = T)) -> final_dta1
 
+#example
+weighted_df <- data %>% 
+  as_survey_design(ids = 1, weight = yourweight)
+
+weighted_df %>% 
+  group_by(racial_group) %>% 
+  summarize(medianHHincome = survey_median(income, na.rm = TRUE)) -> final_income_table
+
 
